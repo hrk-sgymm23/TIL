3bb7f0c67c7b6311af80a2431b956275b84466a9
hrk-sgymm23 <78539910+hrk-sgymm23@users.noreply.github.com>
2024-10-05 23:38:54 +0900

    Create issue_leap.py

---
+++ hackerrank/python/issue_leap.py
@@ -0,0 +1,6 @@
+# https://www.hackerrank.com/challenges/write-a-function/problem?isFullScreen=true
+def is_leap(year):
+    return year % 4 == 0 and (year % 100 == 0 or year % 400 == 0)
+
+year = int(input())
+print(is_leap(year))
