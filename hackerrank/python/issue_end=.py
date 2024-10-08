6e6213340e01d3db60b5dab2faa311f60f53dfe0
hrk-sgymm23 <78539910+hrk-sgymm23@users.noreply.github.com>
2024-10-08 20:54:04 +0900

    Create issue_end=.py

---
+++ hackerrank/python/issue_end=.py
@@ -0,0 +1,9 @@
+# https://www.hackerrank.com/challenges/python-print/problem?isFullScreen=true
+
+# 文字列変換は使えない
+if __name__ == '__main__':
+    n = int(input())
+    
+    result = None
+    for i in range(1, n+1):
+        print(i,end="")
